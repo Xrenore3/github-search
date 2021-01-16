c8ac6eab5e5f2aa21594aa6b0e3d574a71a8c92f
Xrenore3 <romanchaus519@gmail.com>
2021-01-16 10:54:16 +0200

    add to Repos component Pie3D,Column3D,Doughnut2D components with useful data

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,9 +3,38 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  const {repos} = React.useContext(GithubContext);
-  console.log(repos)
-  return <ExampleChart/>;
+  const { repos } = React.useContext(GithubContext);
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+    if (!language) {
+      return total
+    }
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count }
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count
+      }
+    }
+    return total
+  }, {})
+  const mostUsedLanguage = Object.values(languages);
+
+  const mostPorularLanguage = Object.values(languages).map(item=>{
+    const {label,stars} = item;
+    return {label,value:stars}
+  })
+  console.log(mostUsedLanguage)
+  console.log(mostPorularLanguage)
+  return <section className='section'>
+    <Wrapper className='section-center'>
+      <Pie3D data={mostUsedLanguage} />
+      <Column3D data={mostUsedLanguage}/>
+      <Doughnut2D data={mostPorularLanguage} />
+    </Wrapper>
+  </section>;
 };
 
 const Wrapper = styled.div`
